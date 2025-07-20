297395b249e87a4ef1e6e9d94e9e880a23175c67
ssfwmorry <ssfw.morry.galilepitz+develop@gmail.com>
2025-07-20 11:49:01 +0900

    fix(layout): スマホ操作でのダブルタップによる拡大を無効化

--- app.vue
+++ app.vue
@@ -1,10 +1,17 @@
 <template>
-  <v-app>
-    <v-locale-provider locale="ja" class="h-100">
+  <v-app class="app-touch">
+    <v-locale-provider locale="ja">
       <NuxtPwaManifest />
-      <NuxtLayout class="h-100">
+      <NuxtLayout>
         <NuxtPage />
       </NuxtLayout>
     </v-locale-provider>
   </v-app>
 </template>
+
+<style scoped lang="scss">
+.app-touch {
+  // スマホ環境でのダブルタップを無効化する
+  touch-action: manipulation;
+}
+</style>
